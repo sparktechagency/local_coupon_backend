1eeba9309542320912be2fb7f51fe21f6aa80831
safin-sys <mdsafinofficial@gmail.com>
2025-04-07 10:40:37 +0600

    Implement dashboard data retrieval with user and subscription statistics

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -1,11 +1,108 @@
 import { Request, Response } from "express";
 import res from "@utils/response_handler";
-import { Coupon } from "src/db";
+import { Coupon, Payment, User } from "src/db";
 
-const get_dashboard = (req: Request, response: Response) => {
+const get_dashboard = async (req: Request, response: Response) => {
   res.setRes(response);
+  const { subscription_year, user_year } = req.query;
 
-  res.status(420).json({ message: "this is under construction 🚧" });
+  const total_users = await User.countDocuments();
+  const premium_users = await User.countDocuments({ isSubscribed: true });
+  const business_owners = await User.countDocuments({ role: "business" });
+  const coupons = await Coupon.countDocuments();
+
+  const subscriptionData = await Payment.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: new Date(`${subscription_year}-01-01T00:00:00.000Z`),
+          $lte: new Date(`${subscription_year}-12-31T23:59:59.999Z`),
+        },
+      },
+    },
+  ]);
+
+  const subscriptionChart = Array(12).fill(0);
+  subscriptionData.forEach((entry) => {
+    subscriptionChart[entry.createdAt.getMonth() - 1] += 1;
+  });
+
+  const subscription_growth = {
+    year: subscription_year,
+    chart: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      data: subscriptionChart,
+    },
+  };
+
+  const userData = await User.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: new Date(`${user_year}-01-01`),
+          $lte: new Date(`${user_year}-12-31T23:59:59.999Z`),
+        },
+      },
+    },
+  ]);
+
+  const activeUsersChart = Array(12).fill(0);
+  const inactiveUsersChart = Array(12).fill(0);
+
+  userData.forEach((user) => {
+    const monthIdx = user.createdAt.getMonth();
+
+    if (user.isSubscribed) {
+      activeUsersChart[monthIdx] += 1;
+    } else {
+      inactiveUsersChart[monthIdx] += 1;
+    }
+  });
+
+  const user_growth = {
+    year: user_year,
+    chart: {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      legends: ["Active", "Inactive"],
+      data: [activeUsersChart, inactiveUsersChart],
+    },
+  };
+
+  const data = {
+    total_users,
+    premium_users,
+    business_owners,
+    coupons,
+    subscription_growth,
+    user_growth,
+  };
+  res.json({ message: "Dashboard data fetched successfully", data });
 };
 
 const get_recent_transactions = async (req: Request, response: Response) => {
