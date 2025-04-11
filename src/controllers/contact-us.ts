b9e1bede541b82d6576f2ebeaed5023c0380961f
safin-sys <mdsafinofficial@gmail.com>
2025-04-11 11:21:32 +0600

    Refactor contact_us controller to improve response handling and error messaging

--- src/controllers/contact-us.ts
+++ src/controllers/contact-us.ts
@@ -1,14 +1,16 @@
 import { sendEmail } from "@services/emailService";
+import createResponseHandler from "@utils/response_handler";
 import validateRequiredFields from "@utils/validateFields";
 import { Request, Response } from "express";
 
-const contact_us = (req: Request, res: Response) => {
+const contact_us = (req: Request, response: Response) => {
+  const res = createResponseHandler(response);
   const { name, email, subject, message } = req.body || {};
 
   const error = validateRequiredFields({ name, email, subject });
 
   if (error) {
-    res.status(400).json({ error });
+    res.status(400).json({ message: error });
     return;
   }
 
